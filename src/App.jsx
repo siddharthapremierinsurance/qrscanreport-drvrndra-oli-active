5dafbe9587ca54aaa0be66622fd9ff428b051255
santoshthapa321 <santosh.thpa.2080@gmail.com>
2024-06-17 17:34:24 +0545

    bjd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 
 import './App.css'
+import Header from './Header/Header'
 
 function App() {
  
   return (
     <>
-      
+      <Header/>
     </>
   )
 }
---
+++ src/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import image from "../assets/images/sid.png"
+const Header = () => {
+  return (
+    <div className="wholeContainer">
+        <div classname="headerImg">
+            <img src={image} alt="loading"></img>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
